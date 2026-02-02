e49c145bc2bbc2a3900b7c02c79e186be6ffe7c6
DavidZavala <zavaladavid838@gmail.com>
2026-02-02 13:22:40 -0600

    nuevo dominio

--- src/httpClient.jsx
+++ src/httpClient.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 
 // Creamos una instancia de axios configurada
 export default axios.create({
-  baseURL: "https://server-notes-nine.vercel.app", // La URL de tu servidor Flask
+  baseURL: "https://server-notes-beige.vercel.app/", // La URL de tu servidor Flask
   withCredentials: true, // Importante: Permite enviar cookies al servidor
 });
